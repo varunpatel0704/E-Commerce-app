b6779a271d7d8482a52518eee66544820669eff7
electro0704 <118429904+electro0704@users.noreply.github.com>
2024-07-11 14:49:48 +0530

    implement product query controllers

--- backend/src/controllers/product.controllers.js
+++ backend/src/controllers/product.controllers.js
@@ -2,25 +2,43 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.class.js";
 import ApiResponse from "../utils/ApiResponse.class.js";
 import Product from "../models/product.model.js";
+import Category from "../models/category.model.js";
 
-const getTaggedProducts = asyncHandler(async function(req, res, next){
+const getCategories = asyncHandler(async function (req, res, next) {
+  const categories = await Category.find({}).select("-products");
 
+  if (!categories.length) return next(new ApiError(404, "No categories found"));
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Fetched requested categories", categories));
 });
 
-const getCategories = asyncHandler(async function(req, res, next){
+const getProducts = asyncHandler(async function (req, res, next) {
+  // try to fetch categories first and then products under that category.
+  const { category } = req.params || req.body;
+  // utilizing this method coz the (no of categories) << (no of products) and we are already storing products category wise. Hence it makes more sense to fetch then category wise which saves time as well
+  const result = await Category.find({ name: category })
+    .populate("products");
 
+  const products = result.products;
+
+  if (!products.length) return next(new ApiError(404, "No products not found"));
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Fetched requested products", products));
 });
 
-const getProducts = asyncHandler(async function(req, res, next){
-
-});
-
-const getProduct = asyncHandler(async function(req, res, next){
-  const {productId} = req.params;
+const getProduct = asyncHandler(async function (req, res, next) {
+  const { productId } = req.params || req.body;
   const product = await Product.findById(productId);
 
-  if(!product) return next(new ApiError(404, 'Product not found'));
-  
-})
+  if (!product) return next(new ApiError(404, "Product not found"));
 
-export {getLatestProducts, getCategories, getProducts};
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Fetched requested product", product));
+});
+
+export { getCategories, getProducts, getProduct };
--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,41 +1,46 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name:{
-    type: String,
-    required: [true, 'product name is required']
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "product name is required"],
+    },
+
+    category: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        // required: [true, 'product category is required']
+      },
+    ],
+
+    discription: {
+      type: String,
+      default: "No discription provided",
+      // required: [true, 'product discription is required']
+    },
+
+    image: {
+      type: String,
+      required: [true, "product image is required"],
+    },
+
+    stock: {
+      type: Number,
+      required: [true, "product stock is required"],
+    },
+
+    price: {
+      type: Number,
+      required: [true, "product price is required"],
+    },
+    // tags: [{ type: String }], //eg: latest, trending, etc.
+    discount: Number,
   },
+  { timestamps: true }
+);
 
-  category:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category',
-    // required: [true, 'product category is required']
-  },
-
-  discription:{
-    type: String,
-    default: 'No discription provided'
-    // required: [true, 'product discription is required']
-  },
-
-  image:{
-    type: String,
-    required: [true, 'product image is required']
-  },
-
-  stock:{
-    type: Number,
-    required: [true, 'product stock is required']
-  },
-
-  price:{
-    type: Number,
-    required: [true, 'product price is required']
-  },
-  tags:[{type:String}], //eg: latest, trending, etc.
-  discount: Number
-}, {timestamps : true});
-
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
